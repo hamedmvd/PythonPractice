40aaa320786482b45c0bfb97a26af5efb7d0f8d0
Hamed Mohammadvand <70788776+hamedmvd@users.noreply.github.com>
2020-12-13 12:10:05 +0330

    Update ans3.py

--- ans3.py
+++ ans3.py
@@ -1,4 +1,6 @@
-def list_product(list1, list2):
-	return [(i, j)for i in list2 for j in list1]
-	
-list_product(['a', 'b', 'c'], [1, 2])
+def P(coefs,vals):
+    if isinstance(coefs,list):
+        coefs,vals=vals,coefs
+    return [sum([x**i*c for i,c in enumerate(coefs[::-1])]) for x in vals]
+    
+P([1,2,3],(2,-1,1))
